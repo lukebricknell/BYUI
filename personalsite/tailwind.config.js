5b155c32d770cc43b8434f2470c97b65e714ac66
lukebricknell <lukecbricknell@gmail.com>
2022-12-03 21:10:45 +0200

    Services Page

--- personalsite/tailwind.config.js
+++ personalsite/tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./public/index.html", ".public/**/*.{html, js}"],
   theme: {
@@ -6,6 +8,9 @@ module.exports = {
       colors: {
         lred: "#F03E3E",
         beige: "#F8F2DC",
+      },
+      fontFamily: {
+        sans: ["Inter", "sans", ...defaultTheme.fontFamily.sans],
       },
     },
   },
